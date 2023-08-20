f3bfd9b7ae8f82cae17ff651d215b8eac8bb1b91
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2023-08-20 13:43:51 -0400

    put solution 0 in txt

--- simple_job1/generate_inputs.py
+++ simple_job1/generate_inputs.py
@@ -9,34 +9,50 @@
 
 """
 
+import os
+import re
+import sys
+
+
 def replace_solution():
     """replace solution in template file with new solution"""
     pass
+
 
 def update_parameters():
     """update parameters in template file with new parameters"""
     pass
 
-def read_sections (textfile: str) -> list:
 
+def split_on_empty_lines(s: str) -> list:
+
+    # greedily match 2 or more new-lines
+    blank_line_regex = r"(?:\r?\n){2,}"
+
+    return re.split(blank_line_regex, s.strip())
+
+def read_sections(textfile: str) -> list:
     try:
-        with open(textfile, "r",encoding='utf-8') as file:
-            lines = file.readlines()
-        return lines
+        with open(textfile, "r", encoding="utf-8") as file:
+            lines = file.read()
+        # split file into different sections by the empty line
+        sections = split_on_empty_lines(lines)
+        return sections
     except FileNotFoundError:
         print(f"File not found: {textfile}")
         return None
 
+
 def workflow(basefile):
     """generate inputs for simple_job1"""
     sections = read_sections(basefile)
-    print(sections)
+    print(f'Total sections: {len(sections)}')
+
 
 def main():
     basefile = "Beerling_Clean_V2.pqi"
-    workflow(basefile = basefile)
+    workflow(basefile=basefile)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
