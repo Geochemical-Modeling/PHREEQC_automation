9ef82e7d072008d5cff7c0b1a87850e8a7b02040
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2023-08-23 19:46:27 -0400

    rename script

--- simple_job1/batch_run.py
+++ simple_job1/batch_run.py
@@ -1,16 +1,21 @@
 """
-    run_jobs.py
+    batch_run.py
     -- run jobs with multiprocessing
 """
 
 import  argparse
+
+def run_jobs(jobcfg, dryrun):
+    """run jobs with a configuration file"""
+    print("configuration file: ",jobcfg)
+    print("dryrun ? ", dryrun)
 
 def main():
     parser = argparse.ArgumentParser(description="Run jobs in batch.")
     parser.add_argument(dest="job_cfg", type=str, help="job configuration")
     parser.add_argument("-d","--dryrun",action="store_true",help="test with a dryrun")
     args = parser.parse_args()
-    print(args)
+    run_jobs(args.job_cfg, args.dryrun)
 
 if __name__ == "__main__":
     main()
