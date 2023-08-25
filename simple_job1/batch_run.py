68fdbc9fba1da2f5eef136a1b01f343b1d5a963e
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2023-08-25 09:54:52 -0400

    setup logging

--- simple_job1/batch_run.py
+++ simple_job1/batch_run.py
@@ -27,6 +27,10 @@ import sys
 import argparse
 import configparser
 import shutil
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def cfg_to_dict(cfgfile):
     """load cfg file to dictionary"""
@@ -41,9 +45,19 @@ def cfg_to_dict(cfgfile):
     output_dict = {s:dict(config_object.items(s)) for s in config_object.sections()}
     return output_dict
 
+def set_logger(apath):
+    """set the job log at a path"""
+
+    logfile = os.path.join(apath, "run.log")
+    file_handler = logging.FileHandler(logfile)
+    file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_format)
+    logger.addHandler(file_handler)
+
 def prepare_jobs(jobcfg_dict):
     """setup job folders and copy files"""
 
+    jobs_to_run = []
     jobfolder = jobcfg_dict["jobs"]["jobrundir"]
     if os.path.exists(jobfolder):
         shutil.rmtree(jobfolder)
@@ -52,17 +66,21 @@ def prepare_jobs(jobcfg_dict):
     os.makedirs(jobfolder, exist_ok = True)
     print(f"setup run folder: {jobfolder}")
 
+    set_logger(jobfolder)
+    logger.info(f'log for {jobcfg_dict["jobs"]["jobname"]}')
+
     # get the list of input file
     input_dir = jobcfg_dict['jobs']['inputs']
     input_pqi = [x for x in os.listdir(input_dir) if ".pqi" in x ]
-    print(input_pqi)
+    print("total number of jobs: ", len(input_pqi))
 
     # copy files over
     for entry in input_pqi:
         input_name = entry.split(".")[0]
         subfolder = os.path.join(jobfolder,input_name)
+        # record jobs to run
+        jobs_to_run.append({"folder":subfolder, "input":entry})
         if os.path.exists(subfolder):
-            print(subfolder)
             shutil.rmtree(subfolder)
         else:    
             os.makedirs(subfolder, exist_ok = True)
@@ -70,9 +88,10 @@ def prepare_jobs(jobcfg_dict):
         # input.qpi, phreeqcexe, database
         files_to_copy =[os.path.join(input_dir, entry), jobcfg_dict['phreeqc']['phreeqcexe'],jobcfg_dict['jobs']['database']]
         for file in files_to_copy:
-            print(file)
             shutil.copy(file, subfolder)
-        
+    logger.info('setup is done')    
+    
+    return jobs_to_run
 
 def run_jobs(jobcfg, dryrun):
     """run jobs with a configuration file"""
@@ -81,7 +100,8 @@ def run_jobs(jobcfg, dryrun):
 
     jobcfg_dict = cfg_to_dict(jobcfg)
     print(jobcfg_dict)
-    prepare_jobs(jobcfg_dict)    
+    jobs_list = prepare_jobs(jobcfg_dict)    
+    print(jobs_list)
 
 def main():
     parser = argparse.ArgumentParser(description="Run jobs in batch.")
